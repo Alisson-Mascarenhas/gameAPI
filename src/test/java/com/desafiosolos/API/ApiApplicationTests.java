6f6cbba8249b73fcbddb49e206c826086614a02a
Hiltarg <alisson.oliveira@ucsal.edu.br>
2023-05-09 11:41:31 -0300

    Projeto Criado

---
+++ src/test/java/com/desafiosolos/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiosolos.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
