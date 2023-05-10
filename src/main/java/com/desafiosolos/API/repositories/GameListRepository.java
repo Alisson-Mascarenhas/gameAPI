6289279b8ed77cfb7a50de4fbd5b00fff9ee82c5
Alisson <coderbr2020@gmail.com>
2023-05-10 16:52:40 -0300

    Create a List

--- src/main/java/com/desafiosolos/API/repositories/GameListRepository.java
+++ src/main/java/com/desafiosolos/API/repositories/GameListRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.desafiosolos.API.models.GameList;
 
 public interface GameListRepository extends JpaRepository<GameList, Long>{
-
+	
+	GameList findByName(String name);
 }
--- src/main/java/com/desafiosolos/API/services/GameListService.java
+++ src/main/java/com/desafiosolos/API/services/GameListService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.desafiosolos.API.dto.GameDTO;
 import com.desafiosolos.API.dto.GameListDTO;
+import com.desafiosolos.API.models.Game;
 import com.desafiosolos.API.models.GameList;
 import com.desafiosolos.API.repositories.GameListRepository;
 
@@ -16,17 +18,32 @@ public class GameListService {
 
 	@Autowired
 	private GameListRepository gameListRepository;
-	
+
 	@Transactional(readOnly = true)
-	public List<GameListDTO> findAll(){
+	public List<GameListDTO> findAll() {
 		List<GameList> result = gameListRepository.findAll();
-		
+
 		return result.stream().map(x -> new GameListDTO(x)).toList();
 	}
-	
+
 	public GameListDTO findById(Long id) {
-		GameList result = gameListRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Lista não encontrada"));
+		GameList result = gameListRepository.findById(id)
+				.orElseThrow(() -> new NoSuchElementException("Lista não encontrada"));
 		return new GameListDTO(result);
 	}
-	
+
+	@Transactional()
+	public GameListDTO execute(GameListDTO gameListDTO) throws Exception {
+
+		GameList existsGameList = gameListRepository.findByName(gameListDTO.getName());
+		System.out.println(existsGameList);
+		if (existsGameList != null) {
+			throw new Error("Essa lista já existe!");
+		} else {
+			GameList gameList = new GameList(gameListDTO.getName());
+			GameList result = gameListRepository.save(gameList);
+			return new GameListDTO(result);
+		}
+
+	}
 }
